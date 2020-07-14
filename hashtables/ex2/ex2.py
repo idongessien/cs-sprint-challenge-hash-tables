e4d2bb68258cbcc4cd3dfb909cba0b133c37874c
Idong Essien <idong.essien@comcast.net>
2020-07-14 14:30:06 -0400

    ex2 tests passing after 3 tries :crying shame face:

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,26 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    '''init array with the same number elements as tickets'''
+    route = [None] * length
+    
+    '''traverse through tickets to save thier destination'''
+    route_lookup = dict()
+
+    '''traverse through tickets to save thier destination'''
+    for ticket in tickets:
+        
+        route_lookup[ticket.source] = ticket.destination
+    
+    '''Starting ticket has source "NONE" '''
+    next_destination = route_lookup["NONE"]
+
+    '''get ticket new destination & add it to route array break when next destination is string "NONE"'''
+    for current_leg in range(0, length):
+
+        ''' get next destination '''
+        route[current_leg] = next_destination        
+
+        next_destination = route_lookup[next_destination]
+
     return route
