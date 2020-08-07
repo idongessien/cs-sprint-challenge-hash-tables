c081661d25412812a836369ca378c51b53c8b396
Idong Essien <idong.essien@comcast.net>
2020-08-07 12:07:36 -0400

    initial commit ex1 tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,22 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    #new dict
+    get_weights =  dict()
+
+    for weight_index in range(len(weights)):
+
+        current_weight = weights[weight_index]
+
+        # if found ...
+        if current_weight in get_weights:
+
+            # get prev weight
+            prev_weight_index = get_weights[current_weight]
+
+            # return current index tuple
+            return (weight_index, prev_weight_index)
+
+        get_weights[limit - current_weight] = weight_index
 
     return None
