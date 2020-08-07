389462d99a54fc740c6a91a5e78c112ee8cb7901
Idong Essien <idong.essien@comcast.net>
2020-08-07 13:58:22 -0400

    ex3 tests passing. mvp done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,8 +4,22 @@ def intersection(arrays):
     """
     # Your code here
 
-    return result
+    # for numbers that show up in each list...
+    num_instance = dict()
 
+    # for each array in list of arrays...
+    for array in arrays:
+
+        # for each number in array...
+        for num in array:
+
+            if num in num_instance:
+                num_instance[num] += 1
+            else:
+                num_instance[num] = 1
+
+    # return numbers that show up in all arrays...
+    return [result[0] for result in num_instance.items() if result[1] == len(arrays)]
 
 if __name__ == "__main__":
     arrays = []
